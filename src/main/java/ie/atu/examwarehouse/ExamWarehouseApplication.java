a7a9bc90136fa2c65328d99db4e5953355dd9ad3
Denis <G00403126@atu.ie>
2023-11-07 16:24:56 +0000

    .

---
+++ src/main/java/ie/atu/examwarehouse/ExamWarehouseApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.examwarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamWarehouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamWarehouseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/examwarehouse/ExamWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.examwarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamWarehouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
