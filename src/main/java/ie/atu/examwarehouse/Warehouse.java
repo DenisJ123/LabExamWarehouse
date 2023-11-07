2cb63644d60178619e78a0392be1e605d8aff776
Denis <G00403126@atu.ie>
2023-11-07 16:29:25 +0000

    Warehouse

---
+++ src/main/java/ie/atu/examwarehouse/Warehouse.java
@@ -0,0 +1,20 @@
+package ie.atu.examwarehouse;
+
+public class Warehouse {
+
+    private int capacity;
+
+    public Warehouse(int capacity)
+    {
+        this.capacity = capacity;
+    }
+    public int getCapacity()
+    {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity)
+    {
+        this.capacity = capacity;
+    }
+}
